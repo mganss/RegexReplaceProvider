d9a4846f5ccf408cfeda6ca8d79e9ba67ea0b1af
Michael Ganss <michael.ganss@updatestar.com>
2017-04-13 12:44:37 +0200

    Initial commit

---
+++ RegexReplaceProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RegexReplaceProvider")]
+[assembly: AssemblyDescription("IIS Rewrite custom provider to perform regex replacements")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michael Ganss")]
+[assembly: AssemblyProduct("RegexReplaceProvider")]
+[assembly: AssemblyCopyright("Copyright © 2017 Michael Ganss")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22ec6e13-472d-4236-aab1-9e4468c8a0a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RegexReplaceProvider/RegexReplaceProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Web.Iis.Rewrite;
+using System.Text.RegularExpressions;
+
+public class RegexReplaceProvider : IRewriteProvider, IProviderDescriptor
+{
+    string replacement;
+    Regex regex;
+
+    #region IRewriteProvider Members
+
+    public void Initialize(IDictionary<string, string> settings, IRewriteContext rewriteContext)
+    {
+        if (!settings.TryGetValue("Pattern", out string pattern) || string.IsNullOrEmpty(pattern))
+            throw new ArgumentException("Pattern provider setting is required and cannot be empty");
+
+        if (!settings.TryGetValue("Replacement", out replacement) || replacement == null)
+            throw new ArgumentException("Replacement provider setting is required and cannot be null");
+
+        regex = new Regex(pattern);
+    }
+
+    public string Rewrite(string value)
+    {
+        return regex.Replace(value, replacement);
+    }
+
+    #endregion
+
+    #region IProviderDescriptor Members
+
+    public IEnumerable<SettingDescriptor> GetSettings()
+    {
+        yield return new SettingDescriptor("Pattern", "Regex Pattern");
+        yield return new SettingDescriptor("Replacement", "Replacement");
+    }
+
+    #endregion
+}
